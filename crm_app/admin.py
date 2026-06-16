60466da1e6ae16d537115c87f9d8731ab1d2f44a
dhurka832 <dhurka832@gmail.com>
2026-06-16 09:38:25 +0530

    Aligned changes

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import Customer, Lead, FollowUp
 
-# Register your models here.
 admin.site.register(Customer)
 admin.site.register(Lead)
 admin.site.register(FollowUp)
-
-# Admin - Dhurka@345
-# Radhi - Radhi@345
