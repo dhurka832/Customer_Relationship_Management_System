7b04a414f197df7d79dfb5d0b19acca67c7357c1
dhurka832 <dhurka832@gmail.com>
2026-06-19 22:03:26 +0530

    modify urls changes

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -23,7 +23,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("crm_app.urls")),
+    path('crm/',include("crm_app.urls")),
     # path('api/',include("crm_app.api_urls")),
     # path('api/token/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
     # path('api/token/refresh/',TokenRefreshView.as_view(),name="token_refresh")
