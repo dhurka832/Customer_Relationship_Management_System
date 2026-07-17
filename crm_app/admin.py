3bb9f7de29a126f99ba4b17998add17a4aeeb490
dhurka832 <dhurka832@gmail.com>
2026-07-17 15:13:57 +0530

    add demo credentials

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -4,3 +4,6 @@ from .models import Customer, Lead, FollowUp
 admin.site.register(Customer)
 admin.site.register(Lead)
 admin.site.register(FollowUp)
+
+# Admin - Dhurka@345
+# Radhi - Radhi@345 - user
